fc2b47778b8e36c0ba69f3d27802a8adc338d3ba
josh <josh.ribakoff@gmail.com>
2013-09-22 22:49:43 -0400

    Detects if its running in Prestashop, and if so, reads prestashop configuration file to get database details

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -29,12 +29,18 @@ class Module
         define( 'ELITE_CONFIG', dirname(__FILE__).'/config.ini' );
         define( 'ELITE_PATH', '.' );
 
+        switch($this->whichShoppingCart()) {
+            case 'prestashop':
+                require_once $this->shoppingCartRoot().'/config/settings.inc.php';
+                $database = new \VF_TestDbAdapter(array(
+                    'dbname' => _DB_NAME_,
+                    'username' => _DB_USER_,
+                    'password' => _DB_PASSWD_
+                ));
+                break;
+        }
+
         \VF_Singleton::getInstance()->setProcessURL('/modules/vaf/process.php?');
-        $database = new \VF_TestDbAdapter(array(
-            'dbname' => 'prestashop',
-            'username' => 'prestashop',
-            'password' => 'prestashop'
-        ));
         \VF_Singleton::getInstance()->setReadAdapter($database);
     }
 
@@ -52,6 +58,29 @@ class Module
                 ),
             ),
         );
+    }
+
+    /**
+     * Attempts to detect which shopping cart is in the document root (path immediately where VF admin panel is install)
+     * Calculates path to scan based on SCRIPT_FILENAME env variable, so that symlinks are not resolved.
+     * This means even if the admin panel is symlinked in, it finds the path where the symlink exists, not where the admin itself is.
+     */
+    public function whichShoppingCart()
+    {
+        $shoppingCartRoot = $this->shoppingCartRoot();
+
+        $indexCode = file_get_contents($shoppingCartRoot.'index.php');
+
+        if(preg_match('/PrestaShop/', $indexCode)) {
+            return 'prestashop';
+        }
+    }
+
+    // this returns the "simulated" (unresolved) path if run through a symlink
+    public function shoppingCartRoot()
+    {
+        $script = $_SERVER["SCRIPT_FILENAME"];
+        return str_replace('vfadmin/public', '', dirname($script));
     }
 
 }
